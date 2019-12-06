6452b60f1494faa426217f58653d11e0814207f0
dvanrhenen <d.vanrhenen@vwt.digital>
2019-12-06 14:26:10 +0100

    Added loop that checks on logs for 60 seconds. DAT-1440

--- functions/consume_invoices_func/e2e-test/e2e-test.py
+++ functions/consume_invoices_func/e2e-test/e2e-test.py
@@ -4,11 +4,21 @@ import logging
 import sys
 import json
 import uuid
+import time
 
 from google.cloud import storage, pubsub_v1, logging as cloud_logging
 
+PROJECT_ID = sys.argv[1]
 BUILD_ID = sys.argv[2]
 FUNCTION_NAME = sys.argv[3]
+
+
+def time_format(dt):
+    return "%s:%.3f%sZ" % (
+        dt.strftime('%Y-%m-%dT%H:%M'),
+        float("%.3f" % (dt.second + dt.microsecond / 1e6)),
+        dt.strftime('%z')
+    )
 
 
 def provide_files():
@@ -17,8 +27,7 @@ def provide_files():
 
     filename = '{}/e2e-test/e2e_test.pdf'.format(
         config.E2E_STORAGE_BUCKET_FOLDER)
-    blob_exists = storage.Blob(bucket=bucket,
-                               name=filename).exists(st_client)
+    blob_exists = storage.Blob(bucket=bucket, name=filename).exists(st_client)
     if not blob_exists:
         blob = bucket.blob(filename)
         with open("assets/e2e_test.pdf", "rb") as my_file:
@@ -56,33 +65,54 @@ def post_message_to_topic(unique_id):
 
     publisher.publish(topic_name,
                       bytes(json.dumps(message).encode('utf-8')))
+    logging.info('Message for E2E-invoice "{}" has been posted to {}'.format(
+        unique_id, topic_name))
+
+
+def request_log(cloud_logger, unique_id):
+    cur_timestamp = time_format(
+        (datetime.datetime.utcnow() - datetime.timedelta(seconds=60)))
+    log_filter = "severity = INFO " + \
+                 f"AND resource.labels.function_name = " + \
+                 "\"{}\" ".format(FUNCTION_NAME) + \
+                 "AND timestamp >= \"{}\" ".format(cur_timestamp) + \
+                 "AND textPayload: \"[{}]\"".format(unique_id)
+
+    logging.info(log_filter)
+    entries = cloud_logger.list_entries(
+        filter_=log_filter, order_by=cloud_logging.DESCENDING,
+        projects=[PROJECT_ID])
+
+    return entries
 
 
 def get_log_entries(unique_id):
-    # Set Logger client
     cloud_client = cloud_logging.Client()
     log_name = 'cloudfunctions.googleapis.com%2Fcloud-functions'
     cloud_logger = cloud_client.logger(log_name)
 
-    # Set logging filter
-    cur_timestamp = datetime.datetime.utcnow().strftime(
-        "%Y-%m-%dT%H:%M:%S.%f")
-    log_filter = "severity = INFO " + \
-                 f"AND resource.labels.function_name = " + \
-                 "\"{}\" ".format(FUNCTION_NAME) + \
-                 "AND timestamp > {} ".format(cur_timestamp) + \
-                 "AND textPayload: \"[{}]\"".format(unique_id)
+    start_time = time.time()
+    is_existing = {'pdf': False, 'xml': False}
+    while True:
+        if time.time() - start_time > 60:
+            raise TimeoutError('No matching logs found')
+        else:
+            logging.info('Refreshing logs...')
+            entries = request_log(cloud_logger=cloud_logger,
+                                  unique_id=unique_id)
 
-    # Retrieve logs
-    all_entries = cloud_logger.list_entries(
-        page_size=10, filter_=log_filter, order_by=cloud_logging.DESCENDING,
-        projects=[PROJECT_ID])
-    entries = next(all_entries.pages)
+            for entry in entries:
+                logging.info('Found logging: {}'.format(entry.payload))
+                if 'PDF invoice sent' in entry.payload:
+                    is_existing['pdf'] = True
+                elif 'XML invoice sent' in entry.payload:
+                    is_existing['xml'] = True
 
-    # Check if logs contain correct invoice-number
-    for entry in entries:
-        timestamp = entry.timestamp.isoformat()
-        logging.info('* {}: {}'.format(timestamp, entry.payload))
+            if is_existing['pdf'] and is_existing['xml']:
+                logging.info('Both files succesful posted')
+                break
+            else:
+                time.sleep(15.0 - ((time.time() - start_time) % 15.0))
 
 
 if __name__ == '__main__':
@@ -97,5 +127,5 @@ if __name__ == '__main__':
         get_log_entries(unique_id=unique_id)
     except TimeoutError as e:
         logging.error(
-            'An exception occurred: {}.'.format(e))
+            'An timeout occurred: {}.'.format(e))
         sys.exit(1)
